fc44a676afbeaaf3bfec110bc9ffc61326339ffe
KhanMechAI <37394116+KhanMechAI@users.noreply.github.com>
2018-03-21 21:52:38 +1100

    Comment and clean

--- datread.m
+++ datread.m
@@ -1,92 +1,64 @@
 function [Mesh, numNodes,PartArr] = datread(fpath, nodes)
+%% Description
+% This script does much of the pre-proccesing, really its the
+% pre-processing workhorse. datread.m takes the nodes generated in
+% NodeDatRead.m and builds their corresponding elements.
+% It also determines what element type's are in the model and adjusts the
+% connectivity accordingly.
 
-    Mesh =[];
-%     numConnectingPoints = 2;
-    
-    if ~exist('fpath', 'var')
-        fname = 'C:\Users\z5020362\Desktop\Uni\OneDrive_2_9-6-2017\test_files\dp0\SYS\MECH\ds.dat';
-    else
-        fname = [fpath  '\ds.dat'];
-    end
-    
+
+    fname = [fpath  '\ds.dat'];
+
     datafile = fopen(fname);
-
+% Scans down the ds.dat file until the start of the element definitions.
     trashdata = 'a';
     startelements = '/com,*********** Elements';
 
     while ~strncmpi(trashdata,startelements, 25)
         trashdata = fgetl(datafile);
     end
-
+    % This loop runs down until it hits the line where the element type is
+    % stored. The way the data is stored in the ds.dat file for some
+    % elements means that it is necessary to skip a line when reading in
+    % data, hence the skip line variable is a booean value. Tet elements
+    % haven't been added here as yet.
     elid = 'a';
     numNodes = 0;
     while ~strncmpi(elid, 'eblock', 5)
-        elid = strtrim(fgetl(datafile));
+        elid = fgetl(datafile);
         if strncmpi(elid, 'et', 2)
-            ElTypeCheck = strsplit(elid, ',');
-            type = char(ElTypeCheck(3));
-            switch char(ElTypeCheck(3))
-                case {'183'}
-                    numNodes = 8;
-                    skipLine = 0;
-                    xver =@(seedel)  [seedel.nodes(1).xCoordinate,...
-                                      seedel.nodes(5).xCoordinate,...
-                                      seedel.nodes(2).xCoordinate,...
-                                      seedel.nodes(6).xCoordinate,...
-                                      seedel.nodes(3).xCoordinate,...
-                                      seedel.nodes(7).xCoordinate,...
-                                      seedel.nodes(4).xCoordinate,...
-                                      seedel.nodes(8).xCoordinate,...
-                                      seedel.nodes(1).xCoordinate];
-                    yver =@(seedel)  [seedel.nodes(1).yCoordinate,...
-                                      seedel.nodes(5).yCoordinate,...
-                                      seedel.nodes(2).yCoordinate,...
-                                      seedel.nodes(6).yCoordinate,...
-                                      seedel.nodes(3).yCoordinate,...
-                                      seedel.nodes(7).yCoordinate,...
-                                      seedel.nodes(4).yCoordinate,...
-                                      seedel.nodes(8).yCoordinate,...
-                                      seedel.nodes(1).yCoordinate];
-                    numEdges = 4;
-                case {'182'}
-                    numNodes = 4;
-                    skipLine = 0;
-                    xver =@(seedel)  [seedel.nodes(:).xCoordinate,...
-                                        seedel.nodes(1).xCoordinate];
-                    yver =@(seedel)  [seedel.nodes(:).yCoordinate,...
-                                        seedel.nodes(1).yCoordinate];
-                case{'185'}
-                    numNodes = 8;
-                    skipLine = 0;
-                    numEdges = 12;
-                case{'186'}
-                    numNodes = 8;
-                    skipLine = 1;
-                    numEdges = 12;
-            end
+            [skipLine, numNodes, ~, type] = caseCheck(elid);
         end
     end
 
-
-
-
-% %     Code relating to building a mesh is now unused. It is left in case
-% future developements require full mesh visualisation.
-% %     Mesh = nan(numConnectingPoints,numEdges*numel);
-
     temp = strsplit(elid,',');
+    
+    %The number of elements in a part can be read from one of the lines in
+    %the ds.dat file.
     numel = str2double(temp(end));
     
+    %The linetest variable is the variable used to temporarily store and
+    %extract information being read in.
     linetest = fgetl(datafile);
     linetest = fgetl(datafile);
     start = 1;
     numParts = 1;
-%     eltype = char(ElTypeCheck(3));
+    
+    %The part array was a necessary data structure to contain the different
+    %parts that were being meshed independently. It has three attributes,
+    %elements, range and span. Span is a weak attribute technically
+    %speaking as it is a derivative of range.
     PartArr(numParts).elements(numel) = Element1();
     PartArr(numParts).range = [];
     PartArr(numParts).span = numel;
     counter=1;
+    
+    %max_rad stores the maximum distance between any two nodes within an
+    %element thats in the structure. This radius is used later to filter
+    % unecessary tests.
     max_rad = 0;
+    %faceDefinition calculates the face connectivity of an element for the
+    %ray casting algorithm.
     faces = faceDefinition(type);
     while ~strcmpi(linetest, '-1')
         
@@ -98,19 +70,28 @@ function [Mesh, numNodes,PartArr] = datread(fpath, nodes)
             stidx = nums(1);
             PartArr(numParts).range(1) = stidx;
         end
+        
         nodes_nums = nums(2:end);
+        
+        %Extracting the corresponding node objects
         element_nodes = nodes(nodes_nums);
+        
+        %Creating and asssigning nodes to the element object
         PartArr(numParts).elements(counter) = Element1(nums(1), element_nodes,1);
         Faces.A= [];  
         Faces.E1= [];
         Faces.E2= [];
         Faces.ECross= [];
+        
+        %Preprocessing the face triangulation and storing in each element
+        %object.
         for k = 1:size(faces,2)
             face = faces(:,k);
             [~,idx_dup,~] = unique(nodes_nums(face));
             if length(idx_dup) < 3
                 continue
             end
+            
             [a,e1,e2,ecross] = faceTriangulation(element_nodes(face(idx_dup)));
             
             try
@@ -121,16 +102,18 @@ function [Mesh, numNodes,PartArr] = datread(fpath, nodes)
                 
             catch
             end
-            
         end
+        %Assigning faces to the element
         PartArr(numParts).elements(counter).Faces = Faces;
+        %Storing which part the element belongs to
         PartArr(numParts).elements(counter).part_num = numParts;
+        %Stores the radius of influence for the element
         sphere_influence_tracker = PartArr(numParts).elements(counter).sphere_radius;
+        
         if sphere_influence_tracker >= max_rad
             max_rad = sphere_influence_tracker;
         end
-%         Edges = elEdges(elarray(counter),eltype);
-%         Mesh(:, (nums(1)-1)*numEdges+1:nums(1)*numEdges) = Edges;
+
         linetest = fgetl(datafile);
         counter =counter+1;
         
@@ -138,6 +121,8 @@ function [Mesh, numNodes,PartArr] = datread(fpath, nodes)
             linetest = fgetl(datafile);
         end
         
+        %This code just catches the end of a part definition in the ds.dat
+        %file and progresses until all parts are populated.
         if strcmpi(linetest, '-1')
             endidx = nums(1);
             PartArr(numParts).range(2) = endidx;
@@ -162,12 +147,15 @@ function [Mesh, numNodes,PartArr] = datread(fpath, nodes)
             end            
         end
     end
+    %The max rad is used as the first sphere of influence radius, then
+    %local radii are used.
     PartArr(1).maxRadius = max_rad;
     fclose(datafile); 
 end
 
 function [faces] = faceDefinition(element_type)
-
+%% Face Definition
+%Assigns the face definitions to the element type.
 switch lower(element_type)
     case {'168', '187'}
         I = 1;
@@ -185,12 +173,8 @@ switch lower(element_type)
         N = 6;
         O = 7;
         P = 8;
-        
-        
-        
         faces = [[J;I;L;K],[I;J;N;M],[J;K;O;N],[K;L;P;O],[L;I;M;P],[M;N;O;P]];
-        
-        
+          
     case {'182', '181','183'}
         I = 1;
         J = 2;
@@ -200,50 +184,8 @@ switch lower(element_type)
         faces = [J;I;L;K];        
 end
 end
-
-function [Edges] = elEdges(seedel,eltype)
-
-    switch eltype
-        case {'183'}
-            I = 1;
-            J = 3;
-            K = 5;
-            L = 7;
-            E1 = [I; J];
-            E2 = [J; K];
-            E3 = [K; L];
-            E4 = [I; L];
-
-            EdgeIDX = [E1,E2,E3,E4];
-        case{'185', '186'}
-            I = 1;
-            J = 2;
-            K = 3;
-            L = 4;
-            M = 5;
-            N = 6;
-            O = 7;
-            P = 8;
-
-            E1 = [I; J];
-            E2 = [J; K];
-            E3 = [K; L];
-            E4 = [I; L];
-            E5 = [M; N];
-            E6 = [N; O];
-            E7 = [O; P];
-            E8 = [M; P];
-            E9 = [I; M];
-            E10 = [J; N];
-            E11 = [K; O];
-            E12 = [L; P];
-
-            EdgeIDX = [E1,E2,E3,E4,E5,E6,E7,E8,E9,E10,E11,E12];
-    end
-    
-    Edges = seedel.nodenums(EdgeIDX);
-end
 function [skipLine, numNodes, numElements, type] = caseCheck(linetest)
+%Automating the element check
     ElTypeCheck = strsplit(linetest, ',');
     switch char(ElTypeCheck(3))
         case {'183'}
